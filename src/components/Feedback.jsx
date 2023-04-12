63a148938a43058a8c6bd0b8f5802a3f131bf800
Muhammad Junaid <junaid@epochclan.com>
2023-04-12 18:01:18 +0500

    Add testimonials section

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import styles from "../style";
+import { quotes } from "../../assets";
 
-function Feedback() {
+function Feedback({ content, name, title, img }) {
   return (
-    <div>Feedback</div>
-  )
+    <div className="flex justify-between flex-col px-10 py-20 rounded-[20px] max-w[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img src={quotes} alt="quote" className="w-[43px] h-[28px] object-contain" />
+      <p className={`${styles.paragraph} max-w-[280px] my-10`}>{content}</p>
+      <div className="flex">
+        <img src={img} alt={name} className="w-[48px] h-[48px]"/>
+        <div className="ml-10">
+          <p className={`${styles.paragraph} font-bold text-white`}>{name}</p>
+          <p className={`${styles.paragraph} text-dimWhite`}>{title}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Feedback
+export default Feedback;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import styles, { layout } from "../style";
+import { feedback } from "../constants";
+import Feedback from "../components/Feedback";
 
 function Testimonials() {
   return (
-    <div>Testimonials</div>
-  )
+    <section
+      id="testimonials"
+      className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+    >
+      <div className="absolute z-0 w-3/5 h-3/5 -right-1/2 rounded-full blue__gradient"/>
+      <div
+        className={`w-full flex flex-col md:flex-row sm:mb-16 mb-6 relative z-[1] justify-between items-center`}
+      >
+        <h2 className={styles.heading2}>
+          What people are <br className="sm:block hidden" /> saying about us
+        </h2>
+        <div className="w-full mt-6 md:mt-0">
+          <p className={`${styles.paragraph} max-w-[480px] text-left`}>
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+        {feedback.map((feedbackCard, index) => (
+          <Feedback key={feedback.id} {...feedbackCard} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Testimonials
+export default Testimonials;
