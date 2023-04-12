aaffe85fc08d4bf2f23c15a7482b29d259b7e665
Muhammad Junaid <junaid@epochclan.com>
2023-04-12 16:12:56 +0500

    Add CardDeal section and fix button border radius

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({styles}) {
   return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} transition hover:scale-110`}>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} transition hover:scale-110 rounded-xl`}>
       Get Started
     </button>
   )
